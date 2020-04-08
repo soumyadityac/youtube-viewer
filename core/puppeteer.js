4763eba6c45795e07cc3748e04039c2636ab4889
Soumyaditya Chatterjee <schatterjee@tekion.com>
2020-04-08 17:02:57 +0530

    stealth bot

--- core/puppeteer.js
+++ core/puppeteer.js
@@ -1,8 +1,10 @@
 const puppeteer = require('puppeteer');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
 const getBrowserInstance = async (port) => {
   return puppeteer.launch({
-    args: ['--disable-gpu', '--full-memory-crash-report', '--unlimited-storage', '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', `--proxy-server=socks5://127.0.0.1:${port}`],
+    args: [`--proxy-server=socks5://127.0.0.1:${port}`],
     // devtools: true,
   });
 };
